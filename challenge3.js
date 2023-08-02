f103f0bab394fdf93d57e5bdd6288fb500c8766e
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-02 04:51:28 +0200

    Update

---
+++ challenge3.js
@@ -0,0 +1,10 @@
+// It is important to show the following message in order to:
+// - to assure users
+// - to scare hackers
+// - to impress investors
+
+console.warn('Security scan starting');
+
+// It is important to let users know when they can close the page
+
+console.info('Please wait for the scan to complete before closing the browser.');
